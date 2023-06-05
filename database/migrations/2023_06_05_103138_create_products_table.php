71254d871e018534f21b62ee1c2f941bacd320ab
miraj <mirajulislam0724@gmail.com>
2023-06-05 16:53:58 +0600

    MigrationAssignment done

---
+++ database/migrations/2023_06_05_103138_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->float('price', 50);
+            $table->longText('description');
+            $table->integer('quantity')->nullable();
+            $table->string('category', 50);
+
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
